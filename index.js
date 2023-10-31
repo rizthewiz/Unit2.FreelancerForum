cdaa682979b70d77d316c1881b26622472700ed3
rizthewiz <rizthewiz>
2023-10-30 22:22:43 -0400

    need to finish render function

---
+++ index.js
@@ -0,0 +1,73 @@
+const freelancers = [
+  { name: "Alice", price: 30, occupation: "writer" },
+  { name: "Bob", price: 50, occupation: "teacher" },
+  { name: "Carol", price: 70, occupation: "programmer" },
+];
+
+const jobs = [
+  "tutor",
+  "artist",
+  "writer",
+  "mechanic",
+  "carpenter",
+  "escort",
+  "landscaper",
+];
+const nickNames = ["Andy", "Derick", "Richard", "April", "May", "Frank", "Nan"];
+
+const freelancerList = document.getElementById("freelancers");
+const heading = document.querySelector("h1");
+const p = document.querySelector("p");
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function generateRandomFreelancer() {
+  const randomJobIndex = getRandomIndex(jobs);
+  const randomNameIndex = getRandomIndex(nickNames);
+  const randomPrice = Math.floor(Math.random() * 100);
+  const freelancer = {
+    name: nickNames[randomNameIndex],
+    price: randomPrice,
+    occupation: jobs[randomJobIndex],
+  };
+  return freelancer;
+}
+
+function calculateAvg() {
+  const prices = 0;
+  const total = freelancers.reduce(
+    (acc, currentValue) => acc + currentValue.price,
+    prices
+  );
+  return total / freelancers.length;
+}
+
+const intervalId = setInterval(() => {
+  freelancers.push(generateRandomFreelancer());
+  //   render();
+}, 10000);
+
+function render() {
+  //   const freelancerList = document.getElementById("freelancers");
+  console.log(freelancers);
+  console.log(freelancers.name);
+  console.log(freelancerList);
+  const freelancerEls = freelancers.map(() => {
+    console.log(freelancers.name);
+    const listItem = document.createElement("li");
+    listItem.setAttribute("name");
+    listItem.setAttribute("price");
+    listItem.setAttribute("occupation");
+    console.log(listItem);
+  });
+  console.log(freelancerEls);
+  freelancerList.replaceChildren(...freelancerEls);
+}
+
+render();
+//generate
+//render
+// calc and update within render
+// `The avgerage starting pice is ${avg}`;
